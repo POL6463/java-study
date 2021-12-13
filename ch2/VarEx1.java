52213d678c7093d208ae027fc254a25de9620137
POL6463 <kevinkim0617kim@gmail.com>
2021-12-13 23:35:53 +0900

    2장 끝

--- ch2/VarEx1.java
+++ ch2/VarEx1.java
@@ -5,5 +5,11 @@ public class VarEx1 {
 
         System.out.println(year);
         System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
     }
 }
